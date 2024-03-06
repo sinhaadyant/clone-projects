63825a9f433f1c48392194fc34a066f82d855286
sinhaadyant <92117021+sinhaadyant@users.noreply.github.com>
2024-03-07 03:06:07 +0530

    Error fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+// Home.tsx
+
+import React from "react";
 import CarouselBannerWrapper from "@/components/CarouselBannerWrapper";
 import MoviesCarousel from "@/components/MovieCarousel";
-import MovieCarousel from "@/components/MovieCarousel";
 import {
   getPopularMovies,
   getTopRatedMovies,
@@ -9,42 +11,54 @@ import {
 import { SearchResults } from "@/typings";
 import Image from "next/image";
 
-export default async function Home() {
-  const upcomingMovies = await getUpcomingMovies();
-  const topRatedMovies = await getTopRatedMovies();
-  const popularMovies = await getPopularMovies();
+export default function Home() {
+  async function getSearchedMovies(term: string) {
+    const url = new URL("https://api.themoviedb.org/3/search/movie");
+
+    url.searchParams.set("query", term);
+    url.searchParams.set("include_adult", "false");
+    url.searchParams.set("language", "en-US");
+    url.searchParams.set("page", "1");
+
+    const options: RequestInit = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+      },
+      next: {
+        revalidate: 60 * 60 * 24,
+      },
+    };
+
+    const response = await fetch(url.toString(), options);
+    const data = (await response.json()) as SearchResults;
+
+    return data.results;
+  }
+
+  const fetchMoviesData = async () => {
+    const upcomingMovies = await getUpcomingMovies();
+    const topRatedMovies = await getTopRatedMovies();
+    const popularMovies = await getPopularMovies();
+    return { upcomingMovies, topRatedMovies, popularMovies };
+  };
+
   return (
     <main className="">
       <CarouselBannerWrapper />
       <div className="flex flex-col space-y-2 xl:mt-48">
-        <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
-        <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
-        <MoviesCarousel movies={popularMovies} title="Popular" />
+        {/* Fetch movies data */}
+        {fetchMoviesData().then(
+          ({ upcomingMovies, topRatedMovies, popularMovies }) => (
+            <>
+              <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
+              <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
+              <MoviesCarousel movies={popularMovies} title="Popular" />
+            </>
+          )
+        )}
       </div>
     </main>
   );
-}
-export async function getSearchedMovies(term: string) {
-  const url = new URL("https://api.themoviedb.org/3/search/movie");
-
-  url.searchParams.set("query", term);
-  url.searchParams.set("include_adult", "false");
-  url.searchParams.set("language", "en-US");
-  url.searchParams.set("page", "1");
-
-  const options: RequestInit = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
-    },
-    next: {
-      revalidate: 60 * 60 * 24,
-    },
-  };
-
-  const response = await fetch(url.toString(), options);
-  const data = (await response.json()) as SearchResults;
-
-  return data.results;
 }
