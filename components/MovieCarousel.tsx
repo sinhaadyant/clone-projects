2bfa159a025d32d295004c2ed6d10bce119d2c60
sinhaadyant <92117021+sinhaadyant@users.noreply.github.com>
2024-03-07 13:07:30 +0530

    Added Movie Details

--- components/MovieCarousel.tsx
+++ components/MovieCarousel.tsx
@@ -1,6 +1,7 @@
 import { Movie } from "@/typings";
 import MovieCard from "./MovieCard";
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 type Props = { title?: string; movies: Movie[]; isVertical?: boolean };
 
@@ -28,13 +29,47 @@ function MoviesCarousel({ title, movies, isVertical }: Props) {
                 <div className="max-w-2xl">
                   <p className="font-bold">
                     {movie.title} ({movie.release_date?.split("-")[0]})
+                    <Button className="ml-2" variant={"secondary"}>
+                      UA + {movie?.adult == true ? "18" : "9"}
+                    </Button>
                   </p>
                   <hr className="mb-3" />
+                  <p className=" z-20 bottom-5 left-5 flex">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-4 h-4 mt-1 text-yellow-300"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      role="presentation"
+                    >
+                      <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
+                    </svg>{" "}
+                    &nbsp; {movie?.vote_average}
+                    <svg
+                      className="ml-2 w5 h-5 text-gray-800 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M18.1 17.6A7.2 7.2 0 0 1 12 21a6.6 6.6 0 0 1-5.8-3c-2.7-4.6.3-8.8.9-9.7A4.4 4.4 0 0 0 8 4c1.3 1 6.4 3.3 5.5 10.6 1.5-1.1 2.7-3 2.9-6.2 1.4 1 4 5.5 1.7 9.2Z"
+                      />
+                    </svg>
+                    &nbsp; {movie?.popularity}
+                  </p>
+
                   <p className="">{movie.overview}</p>
                 </div>
               </div>
             ))
-          : movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+          : movies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} isVertical />
+            ))}
       </div>
     </div>
   );
